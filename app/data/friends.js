22cb6fde293dc16dc980d5ecd5382979bd0f615a
Locke Monstah <Mloughran87@gmail.com>
2018-12-20 12:48:24 -0500

    most data in place

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Keith Richards",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/KRichards01.JPG/330px-KRichards01.JPG",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Mick Jagger ",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Mick_Jagger_Deauville_2014.jpg/330px-Mick_Jagger_Deauville_2014.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Charlie Watts",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Charlie_Watts_Berlinale_2008.jpg/330px-Charlie_Watts_Berlinale_2008.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Ronnie Wood",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Ron-Wood_in_CA.jpg/330px-Ron-Wood_in_CA.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Bill Wyman",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Bill_Wyman_2009.jpg/330px-Bill_Wyman_2009.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
